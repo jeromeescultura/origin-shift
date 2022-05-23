df319aec821c067bba5a905a2fa0c93a28c2db3a
Jerome Escultura <jescultura@deloitte.com>
2022-05-23 16:55:11 +0800

    Added sample page

--- pages/sample.js
+++ pages/sample.js
@@ -6,7 +6,7 @@ import Question3 from "../components/Question3";
 function sample() {
   return (
     <div className="bg-primaryBG w-full">
-      <div className="w-[80%] mx-auto flex flex-col gap-10 py-20">
+      <div className="w-[80%] mx-auto flex flex-col gap-16 py-20">
         <Question1 />
         <Question2 />
         <Question3 />
