a1c96d504d442f07378a7998325c957a77900274
Jerome Escultura <jescultura@deloitte.com>
2022-05-23 16:52:15 +0800

    Added questions 1-3

---
+++ pages/sample.js
@@ -0,0 +1,21 @@
+import Button from "../components/button";
+import Question1 from "../components/Question1";
+import Question2 from "../components/Question2";
+import Question3 from "../components/Question3";
+
+function sample() {
+  return (
+    <div className="bg-primaryBG w-full">
+      <div className="w-[80%] mx-auto flex flex-col gap-10 py-20">
+        <Question1 />
+        <Question2 />
+        <Question3 />
+        <div>
+          <Button text="Next" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default sample;
