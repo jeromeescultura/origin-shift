6f28a143471595cbeb32cb8013864089be4ae2ae
Jerome Escultura <jescultura@deloitte.com>
2022-05-24 16:30:47 +0800

    fixed buttons issue

--- components/ButtonQuestion.js
+++ components/ButtonQuestion.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 function ButtonQuestion({ text, action }) {
+  const [selected, setSelected] = useState(false);
+  const handleClick = () => {
+    setSelected(!selected);
+  };
   return (
     <button
-      className="w-[203px] h-[48px] bg-white  text-[#505050] font-light text-center border rounded-md focus:bg-highlight focus:border-darkHighlight transition duration-200"
-      onClick={action}
+      className={`${
+        selected ? "z-10" : "z-0"
+      } w-[203px] h-[48px] bg-white  text-[#505050] font-light text-center border rounded-md focus:bg-highlight focus:border-darkHighlight transition duration-200`}
+      onClick={handleClick}
     >
       {text}
     </button>
