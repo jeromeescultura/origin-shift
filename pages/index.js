34208d5244e57431c59df179e1600171850303da
jeromeescultura <102574742+jeromeescultura@users.noreply.github.com>
2022-05-25 16:54:40 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
 
         <div className="absolute top-28 w-[90%] md:w-[80%]">
           <h2 className="text-4xl md:text-[56px] md:leading-[64px] text-primaryText md:max-w-[450px] font-bold ">
-            Business energy that's affordable &amp; future proof
+            Business energy thats affordable &amp; future proof
           </h2>
           <p className="text-secondaryText max-w-[520px] text-[20px] mt-8 font-light">
             By taking our sustainability assessment, we will connect your
@@ -49,7 +49,7 @@ export default function Home() {
               action={handleAssessmentBtn}
             />
             <button className="text-secondaryBG border-secondaryBG border rounded-full py-2 px-8">
-              Let's have a chat
+              Lets have a chat
             </button>
           </div>
         </div>
