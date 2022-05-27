8003751a8e2f5dc0e4b151c8e890393a9069b56f
Jerome Escultura <jescultura@deloitte.com>
2022-05-27 11:11:55 +0800

    Fixed button

--- components/ButtonComponent.js
+++ components/ButtonComponent.js
@@ -1,13 +1,11 @@
 function ButtonComponent({ text, action }) {
   return (
-    <div>
-      <button
-        className="w-full min-w-[200px] bg-secondaryBG text-white rounded-full px-8 py-3 hover:bg-red-600 "
-        onClick={action}
-      >
-        {text}
-      </button>
-    </div>
+    <button
+      className="w-full min-w-[200px] bg-secondaryBG text-white rounded-full px-8 py-3 hover:bg-red-600 "
+      onClick={action}
+    >
+      {text}
+    </button>
   );
 }
 
